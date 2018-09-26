fd3076bcb54b2e16e26faea0e43332149876f5a5
posesop <vposem@gmail.com>
2018-09-26 23:04:37 +0200

    feat: improve post order controller

--- app/controllers/order.js
+++ app/controllers/order.js
@@ -4,13 +4,18 @@ const log = require('../config/log');
 exports.postOrder = async (order) => {
   log.debug('DEBUG: Method controller postOrder, INPUT - ');
   try {
-    const phones = await repository.getPhones();
-    const price = order.phones.reduce((tot, act) => {
-      const phoneOrder = phones.find(phone => phone.name === act.name);
-      return phoneOrder ? tot + phoneOrder.price : 0;
-    }, 0);
-    log.debug(`DEBUG: Method controller postOrder, OUTPUT - Result: ${JSON.stringify(price, null, 2)}`);
-    return { price };
+    const allPhones = await repository.getPhones();
+
+    const phones = allPhones.filter(phone => order.phones.includes(phone.name));
+    const price = phones.reduce((total, current) => total + current.price, 0);
+    
+    const result = {
+      price,
+      ...order,
+      phones,
+    };
+    log.debug(`DEBUG: Method controller postOrder, OUTPUT - Result: ${JSON.stringify(result, null, 2)}`);
+    return result;
   } catch (error) {
     log.error('ERROR: Method controller get phones', error);
     throw error;
